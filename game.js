d40689307e51f14be708187b1eb3f95efa9ede6c
chamith <chamith.dulanga@gmail.com>
2019-12-27 15:25:32 +0530

    commit sample application

---
+++ game.js
@@ -0,0 +1,23 @@
+var gameSettings = {
+  playerSpeed: 200,
+  maxPowerups: 2,
+  powerUpVel: 50,
+}
+
+var config = {
+  width: 260,
+  height: 340,
+  backgroundColor: 0x000000,
+  scene: [Scene1, Scene2],
+  pixelArt: true,
+  physics: {
+    default: "arcade",
+    arcade:{
+        debug: false,
+        debugShowVelocity: false
+    }
+  }
+}
+
+
+var game = new Phaser.Game(config);
